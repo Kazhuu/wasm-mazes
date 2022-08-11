65b49ab0ed9ec17dd84fc4115b1cde3572ac551c
Mauri Mustonen <mauri.mustonen@hotmail.com>
2022-08-11 09:57:50 +0300

    print mazes to pdf and add bootstrap

--- README.md
+++ README.md
@@ -1,7 +1,19 @@
+# wasm-mazes
+
+Generate mazes with WebAssembly directly in your browser to pdf files for easy
+printing, no server needed.
+
+Site is online [here](http://mauri.codes/wasm-mazes/).
+
 ## How to run in debug mode
 
-Builds the project and opens it in a new browser tab. Auto-reloads when the
-project changes.
+Install [Rust](https://www.rust-lang.org/tools/install),
+[wasmpack](https://github.com/rustwasm/wasm-pack) and
+[Node.js](https://nodejs.org/en/) with npm.
+
+Then following command will build the project and open it in a new browser tab.
+Auto-reloads when the project changes.
+
 ```sh
 npm start
 ```
@@ -9,19 +21,7 @@ npm start
 ## How to build in release mode
 
 Builds the project and places it into the `dist` folder.
+
 ```sh
 npm run build
-```
-
-## How to run unit tests
-
-```sh
-# Runs tests in Firefox
-npm test -- --firefox
-
-# Runs tests in Chrome
-npm test -- --chrome
-
-# Runs tests in Safari
-npm test -- --safari
 ```
--- js/index.js
+++ js/index.js
@@ -1,24 +1,90 @@
 import { Maze } from "../pkg";
+import { jsPDF } from "jspdf";
+import { font } from "./courier-new-normal.js"
 
-const pre = document.getElementById("pre");
-const button = document.getElementById("generate-btn");
-const seed_input = document.getElementById("seed");
-const width_input = document.getElementById("width");
-const heigth_input = document.getElementById("height");
+const printButton = document.getElementById("print-btn");
+const easyButton = document.getElementById("easy-btn");
+const mediumButton = document.getElementById("medium-btn");
+const hardButton = document.getElementById("hard-btn");
+const veryHardButton = document.getElementById("very-hard-btn");
+const veryVeryHardButton = document.getElementById("very-very-hard-btn");
+const fontSizeInput = document.getElementById("font-size");
+const seedInput = document.getElementById("seed");
+const widthInput = document.getElementById("width");
+const heightInput = document.getElementById("height");
 
-const parse_input = (input_value, default_value) => {
-  let value = parseInt(input_value, 10);
+const parseInput = (inputValue, defaultValue) => {
+  let value = parseInt(inputValue, 10);
   if (isNaN(value) || value < 1) {
-    value = default_value;
+    value = defaultValue;
   }
   return value;
 }
 
-button.addEventListener("click", event => {
-  const seed = parse_input(seed_input.value, 1);
-  const width = parse_input(width_input.value, 10);
-  const height = parse_input(heigth_input.value, 10);
+const getInputs = () => {
+  const seed = parseInput(seedInput.value, 1);
+  const width = parseInput(widthInput.value, 10);
+  const height = parseInput(heightInput.value, 10);
+  return { width: width, height: height, seed: seed };
+}
+
+const buildMaze = (width, height, seed) => {
   const maze = Maze.new(width, height);
   maze.generate(BigInt(seed));
-  pre.textContent = maze.render();
+  return maze;
+}
+
+const renderToPdf = (maze, fontSize) => {
+  const doc = new jsPDF();
+  doc.addFileToVFS('courier-new-normal.ttf', font);
+  doc.addFont('courier-new-normal.ttf', 'courier-new', 'normal');
+  doc.setFont("courier-new");
+  doc.setFontSize(fontSize);
+  doc.setLineHeightFactor(1);
+  const text = maze.render();
+  const xOffset = doc.internal.pageSize.width / 2;
+  doc.text(text, xOffset, 10, null, null, "center");
+  doc.output('dataurlnewwindow');
+};
+
+printButton.addEventListener("click", () => {
+  const { width, height, seed } = getInputs();
+  const maze = buildMaze(width, height, seed);
+  const fontSize = parseInput(fontSizeInput.value, 10);
+  renderToPdf(maze, fontSize);
+});
+
+easyButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  widthInput.value = 8;
+  heightInput.value = 8;
+  fontSizeInput.value = 25;
+});
+
+mediumButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  widthInput.value = 30;
+  heightInput.value = 30;
+  fontSizeInput.value = 6;
+});
+
+hardButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  widthInput.value = 71;
+  heightInput.value = 130;
+  fontSizeInput.value = 3;
+});
+
+veryHardButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  widthInput.value = 116;
+  heightInput.value = 199;
+  fontSizeInput.value = 2;
+});
+
+veryVeryHardButton.addEventListener("click", (e) => {
+  e.preventDefault();
+  widthInput.value = 232;
+  heightInput.value = 400;
+  fontSizeInput.value = 1;
 });
